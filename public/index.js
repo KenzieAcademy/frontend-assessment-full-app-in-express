205780cd215c60bba3136b9b3d834a40a81419b5
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-07-02 14:51:44 -0400

    added the event listener and and the preventDefault

--- public/index.js
+++ public/index.js
@@ -1,2 +1,12 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+
+document.getElementById('user-create-form').addEventListener('submit', function(event) {
+event.preventDefault();
+let obj = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('mail').value,
+    phone: document.getElementById('phone').value,
+    nickname: document.getElementById('nickname').value
+}
+})
