e1dde2095bba982b368fa527c6773c14be3447ca
Meaghan Crowell <meaghan.crowell@gmail.com>
2018-07-02 17:28:04 -0400

    completed assessment

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.use(express.static(publicFolderPath))
 
 const users = []
 
-// add POST request listener here
+app.post('/api/user', (req, res) => {
+    const foundUser = users.find(user => {
+        return user.username && req.body.username && user.username.toLowerCase() === req.body.username.toLowerCase()
+    })
 
-app.listen(3000);
+    if (!foundUser) {
+        res.status(201);
+        console.log(req.body)
+        req.body.id = Math.floor(Math.random() * 1000);
+        users.push(req.body);
+        res.send(req.body);
+
+    } else {
+        res.status(409);
+        res.send(new Error("Username is taken, try again!"))
+
+    }
+})
+app.listen(3000, () => console.log("Express is running on port 3000"));
--- public/index.js
+++ public/index.js
@@ -1,2 +1,42 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+
+userCreateForm.addEventListener("submit", submitForm);
+
+
+function submitForm(event) {
+    event.preventDefault();
+
+    let user = {
+
+        name: document.getElementById("name").value,
+        birthdate: document.getElementById("DOB").value,
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+
+    }
+    console.log(user)
+
+    const postMethod = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+
+    };
+
+
+    fetch("http://localhost:3000/api/user", postMethod)
+
+        .then(async function (response) {
+            if (!response.ok) {
+                throw await response.json();
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.log('Error:', error));
+};
