b19d014e1e42d44fdd0b7cb82956f8399ab61558
TJ Hindman <thind131@gmail.com>
2018-07-23 12:16:33 -0400

    functionality finished, need work appending user

--- public/main.js
+++ public/main.js
@@ -2,10 +2,13 @@ const backColDivWrap = document.getElementById("bck-col-div");
 
 function populateColDiv() {
 
-    for ( let colDiv = 0; colDiv < 5; colDiv++ ) {
+    for ( let colDiv = 0; colDiv < 6; colDiv++ ) {
         const newColDiv = document.createElement("div");
-        
+
+        newColDiv.id = `c${colDiv}`;
         newColDiv.classList.add("col-div-style");
-        backColDivWrap.appendChild(backColDivWrap);
+        backColDivWrap.appendChild(newColDiv);
     }
 }
+
+populateColDiv();
