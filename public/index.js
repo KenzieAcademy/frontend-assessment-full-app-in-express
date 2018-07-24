01eee2518f1531f9be7fa1a105788192902210f0
bschue <bschuess3@gmail.com>
2018-07-24 16:24:11 -0400

    first commit

--- public/index.js
+++ public/index.js
@@ -1,2 +1,24 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+userCreateForm.addEventListener("submit",submitUserCreate)
+
+
+
+function submitUserCreate(event) {
+    event.preventDefault()
+    User = {
+        userName: document.getElementById("newUserName").value,
+        email: userCreateForm.email.value,
+        password: userCreateForm.password.value,
+        newPassword: userCreateForm.newPassword.value
+    }
+    fetch("/api/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(User),
+        }).then(res => res.json())
+        .catch(error => console.error("Error:", error))
+        .then(response => console.log("Success:", response));
+}
