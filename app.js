227b9343120d73ce5849808a709d0202bae80f3c
Patrick Hanson <patrickhanson08@gmail.com>
2018-10-14 16:28:02 -0400

    finished functionality

--- app.js
+++ app.js
@@ -10,5 +10,17 @@ app.use(express.static(publicFolderPath))
 const users = []
 
 // add POST request listener here
+app.post('/api/users', function(req, res) {
+    console.log(req.body)
+    if(users.find(user => user.username === req.body.username)) {
+        res.status(409)
+        res.send(new Error('Username already exists'))
+    } else {
+        req.body.userid = Math.floor(Math.random() * 1000)
+        users.push(req.body)
+        res.status(201).send(req.body)
+    }
+    console.log(users)
+} )
 
 app.listen(3000);
--- public/index.js
+++ public/index.js
@@ -1,2 +1,23 @@
 const userCreateForm = document.getElementById("user-create-form")
-const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+const userCreateSubmitButton = userCreateForm.querySelector("button")
+
+userCreateSubmitButton.addEventListener("click", submitFormInfo)
+
+function submitFormInfo(event) {
+    event.preventDefault()
+    const formElement = event.currentTarget.parentElement
+    const User = {
+        username: formElement.querySelector('#username').value,
+        email: formElement.querySelector('#email').value,
+        birthdate: formElement.querySelector('#birthdate').value,
+        password: formElement.querySelector('#password').value
+    }
+    fetch('//localhost:3000/api/users', {
+        method: 'POST',
+        body: JSON.stringify(User),
+        headers: {'Content-type': 'application/json'}
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    
+}
